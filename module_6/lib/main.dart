bcac8262bc21d8a57a8234e9b5ce99221e9607eb
nsaqib1 <nsaqib32@gmail.com>
2023-09-16 23:10:03 +0600

    asas

--- module_6/lib/main.dart
+++ module_6/lib/main.dart
@@ -10,6 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Module 6 Live Test',
       home: HomeScreen(),
     );
@@ -21,6 +22,41 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text("My Shopping List"),
+        actions: const [
+          Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Icon(Icons.shopping_cart),
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          ListTile(
+            leading: Icon(Icons.home),
+            title: Text("Home"),
+          ),
+          ListTile(
+            leading: Icon(Icons.shop),
+            title: Text("Shop"),
+          ),
+          ListTile(
+            leading: Icon(Icons.apple),
+            title: Text("Apple"),
+          ),
+          ListTile(
+            leading: Icon(Icons.star),
+            title: Text("Star"),
+          ),
+          ListTile(
+            leading: Icon(Icons.sunny),
+            title: Text("Sunny"),
+          ),
+        ],
+      ),
+    );
   }
 }
