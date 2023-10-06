d494cc9ff1ab914964b7acf5b6e740ea880d0e96
nsaqib1 <nsaqib32@gmail.com>
2023-10-06 23:13:09 +0600

    asas

--- module_9/lib/main.dart
+++ module_9/lib/main.dart
@@ -10,6 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Module 9 Live Test',
       home: HomeScreen(),
     );
@@ -24,8 +25,83 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  int _index = 0;
+
+  _handleClick(int index, String msg) {
+    _index = index;
+    setState(() {});
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(msg),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text("Size Selector"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(2),
+        child: Center(
+          child: Wrap(
+            spacing: 10,
+            children: [
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  elevation: 0,
+                  backgroundColor: _index == 0 ? Colors.amber : Colors.grey,
+                ),
+                onPressed: () => _handleClick(0, "S"),
+                child: const Text("S"),
+              ),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  elevation: 0,
+                  backgroundColor: _index == 0 ? Colors.amber : Colors.grey,
+                ),
+                onPressed: () => _handleClick(0, "S"),
+                child: Text("S"),
+              ),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  elevation: 0,
+                  backgroundColor: _index == 0 ? Colors.amber : Colors.grey,
+                ),
+                onPressed: () => _handleClick(0, "S"),
+                child: Text("S"),
+              ),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  elevation: 0,
+                  backgroundColor: _index == 0 ? Colors.amber : Colors.grey,
+                ),
+                onPressed: () => _handleClick(0, "S"),
+                child: Text("S"),
+              ),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  elevation: 0,
+                  backgroundColor: _index == 0 ? Colors.amber : Colors.grey,
+                ),
+                onPressed: () => _handleClick(0, "S"),
+                child: Text("S"),
+              ),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  elevation: 0,
+                  backgroundColor: _index == 0 ? Colors.amber : Colors.grey,
+                ),
+                onPressed: () => _handleClick(0, "S"),
+                child: Text("S"),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
