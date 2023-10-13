5fc12aca7593980009cb14999a05a29d874284c2
nsaqib1 <nsaqib32@gmail.com>
2023-10-13 17:07:09 +0600

    module 10 live test preparation

---
+++ module_10/lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Module 10 Live Test',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    DateFormat("hh:mm a dd-MM-yyyy").format(DateTime.now());
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("data"),
+      ),
+    );
+  }
+}
